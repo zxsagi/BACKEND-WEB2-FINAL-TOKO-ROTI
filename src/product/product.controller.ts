6518275b58eedaf0d25b2ff63e64b6ed291cba4d
Sagi Setiono <2382016@unai.edu>
2025-04-30 23:49:05 +0700

    Update product.controller.ts

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,39 +2,39 @@ import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common'
 import { ProductService } from './product.service';
 import { CreateProductDto } from './create-product.dto';
 import { UpdateProductDto } from './update-product.dto';
-import { Public } from '../auth/public.decorator'; // ⬅️ Tambahkan ini
+import { Public } from '../auth/public.decorator'; // Jika kamu pakai AuthGuard
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Public()
   @Post()
+  @Public() // opsional, hanya jika kamu pakai guard global
   create(@Body() body: CreateProductDto) {
-    console.log('DATA DITERIMA:', body);
+    console.log('📥 DITERIMA DI CONTROLLER:', body);
     return this.productService.create(body);
   }
 
-  @Public()
   @Get()
+  @Public()
   findAll() {
     return this.productService.findAll();
   }
 
-  @Public()
   @Get(':id')
+  @Public()
   findOne(@Param('id') id: number) {
     return this.productService.findOne(id);
   }
 
-  @Public()
   @Put(':id')
+  @Public()
   update(@Param('id') id: number, @Body() data: UpdateProductDto) {
     return this.productService.update(id, data);
   }
 
-  @Public()
   @Delete(':id')
+  @Public()
   remove(@Param('id') id: number) {
     return this.productService.remove(id);
   }
